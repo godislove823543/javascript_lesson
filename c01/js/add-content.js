4759432c96f1890d8cc623472ed390681209e996
Sc <godislove823543@gmail.com>
2023-05-08 11:18:28 +0800

    去tsconfig改root和output

---
+++ c01/js/add-content.js
@@ -0,0 +1,4 @@
+var my_name = "Sc"; //
+console.log(my_name);
+var first_name = "Chang";
+console.log(first_name);
---
+++ c01/ts/add-content.ts
@@ -0,0 +1,4 @@
+let my_name = "Sc"; //
+console.log(my_name);
+var first_name = "Chang"
+console.log(first_name);
