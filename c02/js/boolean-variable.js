66e449f2e73d116bb81af02edf6144a53682807f
Sc <godislove823543@gmail.com>
2023-05-08 16:50:50 +0800

    用nb 寫字串插補

--- c02/js/boolean-variable.js
+++ c02/js/boolean-variable.js
@@ -7,3 +7,4 @@ let stock_span = document.getElementById('stock');
 let shipping_span = document.getElementById('shipping');
 stock_span.className=inStock.toString()
 shipping_span.className=shipping
+
