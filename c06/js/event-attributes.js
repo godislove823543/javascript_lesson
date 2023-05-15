6414a9467abda67517404ca8e69fd33e5b26a907
Sc <godislove823543@gmail.com>
2023-05-15 14:18:04 +0800

    觸發使用者填寫格子

--- c06/js/event-attributes.js
+++ c06/js/event-attributes.js
@@ -1,10 +1,13 @@
 function checkUsername(){
     let feedbackElement = document.getElementById('feedback')
-    let usernameElement = document.getElementById('username')
-    let username = usernameElement.value
+    
+    let username = this.value
     if(username.length < 5){
         feedbackElement.textContent = "使用者的名稱必需大於5個字元"
     }else{
         feedbackElement.textContent =  ""
     }
 }
+//html handler
+let usernameElement=document.getElementById('username')
+usernameElement.onblur=checkUsername
