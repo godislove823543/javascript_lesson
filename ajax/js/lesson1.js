9c619eaf293a66ef55a5388d20eb10c766866aa2
Sc <godislove823543@gmail.com>
2023-05-16 16:36:33 +0800

    點按後有這個行政區

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,12 +1,26 @@
+let sarea_array=[]
 let sareaElement=document.getElementById('sarea')
+
+sareaElement.addEventListener('change',(event)=>{
+    let selectedIndex=sareaElement.selectedIndex;
+    selectedValue=sareaElement.options[selectedIndex].value
+    if(sarea_array.includes(selectedValue)){
+        console.log('有這個行政區')
+    }
+    // console.log(sareaElement.options[selectedIndex].value)
+    // sareaElement.options[]
+});
 function reqListener() {
     let youbikedata=JSON.parse(this.responseText);
-    let sarea_array=[]
+    // let sarea_array=[]
     //抓站點資料
     for(const youbike of youbikedata){
         sarea_array.push(youbike.sarea)
     }
     sarea_array=[...new Set(sarea_array)] //set裡的資料不能重複
+    let optionElement=document.createElement('option')
+    optionElement.textContent='請選擇行政區'
+    sareaElement.appendChild(optionElement)
     // console.log(sarea_array)
     //一個一個抓
     for(const area of sarea_array){
