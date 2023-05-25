2a3ac41b1b5550279e3116efeadfdb37e69c5f71
Sc <godislove823543@gmail.com>
2023-05-25 16:09:25 +0800

    收集 填寫的ˊ資料

--- product/public/js/index.js
+++ product/public/js/index.js
@@ -1,13 +1,25 @@
 let formElement=document.querySelector('#form') //選id要用#
 
+let allOfDataArray=[];
 
 const validateName=()=>{ //驗證name函數
     let inputNameElement=document.querySelector('#inputName')
     let productName=inputNameElement.value
-        console.log(productName) //看主控台 填資料 點送出按鈕
+    let nameAlertElement=document.querySelector('#nameAlert')
+    //沒填資料的話 顯示警告訊息
+    if(productName.length ==0){
+       //看主控台 填資料 點送出按鈕
+        console.log("沒有填資料")
+        nameAlertElement.classList.remove('close') //把alert close的close移除
+        return  //沒有填寫就跳出 
+    }
+      console.log(productName) 
+      allOfDataArray.push({'productName':productName} )//key:value
 }
+
 
 formElement.addEventListener('submit',(event)=>{
     event.preventDefault() //先不要動
     validateName()
+    console.log(allOfDataArray) //收集填寫的資料
 })
