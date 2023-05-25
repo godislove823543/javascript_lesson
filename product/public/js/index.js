7dccc1020e78a2a0284eecb962116d8b5f182e17
Sc <godislove823543@gmail.com>
2023-05-25 16:35:51 +0800

    改 產品編號 輸入框的功能

--- product/public/js/index.js
+++ product/public/js/index.js
@@ -21,13 +21,30 @@ const clearAllAlertAndData=()=>{
     //清除產品警告
     let nameAlertElement=document.querySelector('#nameAlert')
     nameAlertElement.classList.add("close")
+
+    //清除code
+
     //清除收集的資料(舊資料 警告訊息)
     allOfDataArray=[]
+}
+
+const validateCodeFormat=()=>{ //在 產品編號那
+    let inputCodeElement=document.querySelector('#inputCode')
+    let inputCodeValue=inputCode.value //輸入資料
+    console.log(inputCodeValue)
+    if(inputCodeValue.length ==0){
+        //看主控台 填資料 點送出按鈕
+         console.log("沒有填資料")
+         inputCodeElement.classList.remove('close') //把alert close的close移除
+         return  //沒有填寫就跳出 
+     }
+     allOfDataArray.push('code':inputCodeValue) //把輸入的資料放進陣列李
 }
 
 formElement.addEventListener('submit',(event)=>{
     clearAllAlertAndData()
     event.preventDefault() //先不要動
     validateName()
+    validateCodeFormat()
     console.log(allOfDataArray) //收集填寫的資料
 })
