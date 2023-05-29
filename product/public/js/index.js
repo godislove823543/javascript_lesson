06ee45247b1e807f8faec367c444d0237fafcece
Sc <godislove823543@gmail.com>
2023-05-29 11:45:13 +0800

    設定欄位功能

--- product/public/js/index.js
+++ product/public/js/index.js
@@ -1,6 +1,25 @@
 let formElement = document.querySelector('#form')
-
 let allOfDataArray = [];
+
+
+const setupCurrentDate = () => {
+    let current = new Date()
+    let year = current.getFullYear()
+    let month = current.getMonth()+1
+    let date = current.getDate()
+
+
+    let month_string = String(month).padStart(2,"0")
+    let date_string = String(date).padStart(2,"0")
+    let current_date_string = `${year}-${month_string}-${date_string}`
+
+    let dateElement = document.querySelector('#warrantyDate')
+    dateElement.value = current_date_string
+    dateElement.min = current_date_string //讓最近日期為今日
+}
+
+//setup現在的日期
+setupCurrentDate()
 
 const validateName = ()=>{
     let inputNameElement = document.querySelector('#inputName')
@@ -18,14 +37,41 @@ const validateCodeFormat = () =>{
     let inputCodeElement = document.querySelector('#inputCode')
     let codeAlertElement = document.querySelector('#codeAlert')
     let inputCodeValue = inputCodeElement.value
-    console.log(inputCodeValue)
-    if (inputCodeValue.length == 0){
+    
+    //正規則表達式
+    const codePatternReg = /\w\w\w-\w\w\w-\w\w\w/g
+
+    if (inputCodeValue.length == 0 || codePatternReg.test(inputCodeValue) == false){
         //console.log("沒有填資料")
         codeAlertElement.classList.remove("close")
+        console.log("執行錯誤")
         return
     }
     allOfDataArray.push({'code':inputCodeValue})
 }
+
+const checkRadioValue = () => {
+    let radionElements = document.querySelectorAll('.form-check-input')
+    radionElements.forEach(element => {
+        if(element.checked){
+            //console.log(element.value)
+            allOfDataArray.push({'catgory':element.value})
+        }
+    })
+}
+
+const warrantyCheck = () =>{
+    let checkboxElement = document.querySelector('#warrantyCheck1')
+    if (checkboxElement.checked){
+        allOfDataArray.push({'warranty':true})
+    }else{
+        allOfDataArray.push({'warranty':false})
+    }
+}
+
+const getWarrantyDate=()=>{
+    let dateElement = document.querySelector('#warrantyDate')
+    allOfDataArray.push({'warrantyDate':dateElement.value})
 
 const clearAllAlertAndData = ()=>{
     //清除產品警告
@@ -45,5 +91,8 @@ formElement.addEventListener('submit',(event)=>{
     event.preventDefault()
     validateName()
     validateCodeFormat()
+    checkRadioValue()
+    warrantyCheck()
+    getWarrantyDate()
     console.log(allOfDataArray)
 })
