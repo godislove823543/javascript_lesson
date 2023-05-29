ff8cfdf6118257a0c6dc66948a4a13f8d3af10c5
Sc <godislove823543@gmail.com>
2023-05-29 14:27:23 +0800

    将 Firebase 添加至您的 JavaScript 项目的其他方法/去專案設定下面的cdn複製咚咚

--- product/public/js/index.js
+++ product/public/js/index.js
@@ -1,3 +1,18 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDB1nEikfSyia4yWS76A6wKmcsP9SR2Xrk",
+    authDomain: "project-javascript-f5727.firebaseapp.com",
+    projectId: "project-javascript-f5727",
+    storageBucket: "project-javascript-f5727.appspot.com",
+    messagingSenderId: "986386632957",
+    appId: "1:986386632957:web:61380e21f543995436ae94",
+    measurementId: "G-3DKN0JY6VR"
+  };
+  const app = initializeApp(firebaseConfig);
+  
 let formElement = document.querySelector('#form')
 let allOfDataArray = [];
 
